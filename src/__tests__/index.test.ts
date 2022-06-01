d33d956c4a43499e53884430d5185d7e8252f004
Sanjay Soundarajan <sanjay.16@live.com>
2022-06-01 15:53:00 -0700

    fix: update tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,10 +1,14 @@
 import test from "ava";
 import { foo, bar } from "../index";
 
-test("foo()", (t) => {
-  t.is(foo(1, 2), 3);
+test("add()", (t) => {
+  t.is(add(1, 2), 3);
 });
 
-test("bar()", (t) => {
-  t.is(bar(2, 1), 1);
+test("subtract()", (t) => {
+  t.is(subtract(2, 1), 1);
+});
+
+test("multiply()", (t) => {
+  t.is(multiply(2, 6), 12);
 });
